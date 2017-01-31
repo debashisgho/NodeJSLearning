7397d81257ff88813cc21e07a83e06621748d7d5
Debashis Ghosh <debashisgho@gmail.com>
2017-01-31 10:17:06 +0530

    completed book add functionality

--- 0_apps/bookstore/public/controllers/books.js
+++ 0_apps/bookstore/public/controllers/books.js
@@ -16,14 +16,14 @@ myApp.controller('BooksController', ['$scope', '$http', '$location', '$routePara
 		});
 	}
 
-/*
+
 	$scope.addBook = function(){
 		console.log($scope.book);
-		$http.post('/api/books/', $scope.book).success(function(response){
+		$http.post('/bookstore/api/books/', $scope.book).then(function(response){
 			window.location.href='#/books';
 		});
 	}
-
+/*
 	$scope.updateBook = function(){
 		var id = $routeParams.id;
 		$http.put('/api/books/'+id, $scope.book).success(function(response){
