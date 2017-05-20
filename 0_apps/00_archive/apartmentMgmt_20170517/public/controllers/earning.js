ce2a001f30d65a7e78f55a739223df871b36776a
Debashis Ghosh <debashisgho@gmail.com>
2017-05-20 07:42:27 +0530

    replaced bootstrap.js with ui.bootstrap and some cleaning

--- 0_apps/00_archive/apartmentMgmt_20170517/public/controllers/earning.js
+++
@@ -1,8 +0,0 @@
-var myApp = angular.module('myApp');
-
-myApp.controller('EarningController', ['$scope', '$http', '$location', '$routeParams','SessionService', function($scope, $http, $location, $routeParams,SessionService){
-	console.log('EarningController loaded...');
-
-	SessionService.runInitialSetUp();
-
-}]);
--- 0_apps/00_archive/apartmentMgmt_20170517/public/controllers/home_LoggedIn.js
+++
@@ -1,8 +0,0 @@
-var myApp = angular.module('myApp');
-
-myApp.controller('Home_LoggedInController', ['$scope', '$http', '$location', '$routeParams','SessionService', '$rootScope', function($scope, $http, $location, $routeParams,SessionService,$rootScope){
-	console.log('Home_Controller loaded...');
-	
-	SessionService.runInitialSetUp();
-	
-}]);
